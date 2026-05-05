f19a33177115509cc5ec292716dfd0a461c333a9
PhamDanhThai <thaipham130406@gmail.com>
2026-05-05 15:31:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 
-
 BLOCK_SIZE = 64
 
 def zero_pad(s):
@@ -11,14 +10,17 @@ def zero_pad(s):
 def split_blocks(s):
     return [s[i:i+BLOCK_SIZE] for i in range(0, len(s), BLOCK_SIZE)]
 
+def normalize_key(key):
+    if len(key) < 64:
+        key = key.ljust(64, '0')
+    elif len(key) > 64:
+        key = key[:64]
+    return key
+
 def encrypt_block(block, key):
     result = ""
     for i in range(64):
-        result += '1' if block[i] != key[i % len(key)] else '0'
-
-    if len(result) != 64:
-        result = result[:64].ljust(64, '0')
-
+        result += '1' if block[i] != key[i] else '0'
     return result
 
 def main():
@@ -27,19 +29,14 @@ def main():
         return
 
     plaintext = data[0]
-    key = data[1]
+    key = normalize_key(data[1])
 
     plaintext = zero_pad(plaintext)
     blocks = split_blocks(plaintext)
 
     result = ""
     for b in blocks:
-        cipher = encrypt_block(b, key)
-
-        if len(cipher) != 64:
-            cipher = cipher[:64].ljust(64, '0')
-
-        result += cipher
+        result += encrypt_block(b, key)
 
     print(result, end="")
 
